f5e9eabc409713a92ae78f2ac3cdcaf2f787966d
ahmed isiaka <ahmeddavids6@gmail.com>
2025-02-06 15:47:42 +0100

    Updated

---
+++ festac_hub/sequelize_part2/migrations/20250122102417-create-football-clubs.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('FootballClubs', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      ucl: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      stadium: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      coach: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      topsix: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('FootballClubs');
+  }
+};
