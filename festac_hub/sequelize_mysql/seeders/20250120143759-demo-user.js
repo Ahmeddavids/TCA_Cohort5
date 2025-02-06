04f55b6a2cb397c29eb6a00c4885daf132050a00
ahmed isiaka <ahmeddavids6@gmail.com>
2025-02-06 15:43:55 +0100

    Updated

---
+++ festac_hub/sequelize_mysql/seeders/20250120143759-demo-user.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'example@example.com',
+        age: 32,
+        password: "12345",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  },
+};
