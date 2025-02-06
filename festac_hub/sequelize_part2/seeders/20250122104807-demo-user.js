707120ab430e34d0ca4583d94c9d3ddcc616d8db
ahmed isiaka <ahmeddavids6@gmail.com>
2025-02-06 15:49:37 +0100

    Updated

---
+++ festac_hub/sequelize_part2/seeders/20250122104807-demo-user.js
@@ -0,0 +1,21 @@
+const { v4: uuid } = require('uuid')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('FootballClubs', [
+      {
+        id: uuid(),
+        name: "Brentford",
+        stadium: "Griffin Park",
+        coach: "Thomas Frank",
+        topsix: false,
+        ucl: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('FootballClubs', null, {});
+  },
+};
