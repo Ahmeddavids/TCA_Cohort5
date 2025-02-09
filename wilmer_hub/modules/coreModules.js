ed80af270cb096a3befdd97441ab4dcaf0c122bb
ahmed isiaka <ahmeddavids6@gmail.com>
2025-02-09 15:49:13 +0100

    Modules

---
+++ wilmer_hub/modules/coreModules.js
@@ -0,0 +1,10 @@
+// const http = require('http')
+
+// http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('Welcome to the Backend Class');
+//     res.end();
+// }).listen(2024);
+
+
+
