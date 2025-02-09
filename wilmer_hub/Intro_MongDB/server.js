391ce2c530136217ea2d836d4c69557a145def30
ahmed isiaka <ahmeddavids6@gmail.com>
2025-02-09 15:50:51 +0100

    Added

---
+++ wilmer_hub/Intro_MongDB/server.js
@@ -0,0 +1,13 @@
+require('./config/database')
+const express = require('express');
+const app = express();
+const studentRouter = require('./routes/studentRouter');
+const PORT = 2121;
+
+app.use(express.json());
+
+app.use(studentRouter);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening to port ${PORT}`)
+});
