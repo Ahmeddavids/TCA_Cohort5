5a7f8b9306acebd19e226db097dc70596201cb05
ahmed isiaka <ahmeddavids6@gmail.com>
2025-02-09 15:48:34 +0100

    Script

---
+++ wilmer_hub/modules/script.js
@@ -0,0 +1,13 @@
+// const arithemetic = require('./localModules');
+
+// Destructuring
+const { multiply } = require('./localModules');
+
+// console.log(arithemetic.add(5,9));
+// console.log(arithemetic.divide(8,2))
+
+// console.log('Add: ',add(7851,6835));
+// console.log('Subtract: ',subtract(7851,6835));
+console.log('Multiply: ',multiply(7851,6835));
+// console.log('Divide: ',divide(7851,6835));
+
