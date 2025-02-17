342fd4f864cba1ee558948360d9f41007e14f729
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-17 20:06:22 +0100

    Database

---
+++ cloudinary_multer/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+const DB = process.env.MONGODB_URI;
+
+mongoose.connect(DB)
+.then(()=> {
+    console.log('Connection to database is successful')
+})
+.catch((error)=> {
+    console.log('Error connection to database' + error.message)
+});
