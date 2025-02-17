db250ca8ad5575308ffb2fa0c8daba35f72256a6
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-17 14:37:35 +0100

    Server

---
+++ wilmer_hub/Multiple-Image-Multer/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+require('./config/database');
+const userRouter = require('./routes/userRouter');
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening to PORT: ${PORT}`)
+})
