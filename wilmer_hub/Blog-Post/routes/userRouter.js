a7466b65391139101122498bb524a65e7a4a9a47
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-20 23:27:21 +0100

    dp ssk

--- wilmer_hub/Blog-Post/routes/userRouter.js
+++ wilmer_hub/Blog-Post/routes/userRouter.js
@@ -1,8 +1,10 @@
-const { register } = require('../controllers/userController');
+const { register, login } = require('../controllers/userController');
 const upload = require('../utils/multer');
 
 const router = require('express').Router();
 
 router.post('/register', upload.single('profilePic'), register);
+
+router.post('/login', login);
 
 module.exports = router;
