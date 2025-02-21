44fdf83ac9c70df6b8d042535812f419a75d99e6
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-21 19:25:36 +0100

    Server

---
+++ authentication/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('./config/database');
+const userRouter = require('./routes/userRouter');
+
+const PORT = process.env.PORT || 2019;
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/v1',userRouter);
+
+app.listen(PORT, ()=> {
+    console.log(`Server is listening to PORT: ${PORT}`);
+});
