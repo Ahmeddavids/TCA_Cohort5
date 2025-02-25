863d20ba597f9a4cae71288d20844faaf09b2670
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-25 17:20:45 +0100

    Added forgot and reset route

--- authentication 2/routes/userRouter.js
+++ authentication 2/routes/userRouter.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const { register, login, verifyUser, resendVerificationEmail } = require('../controllers/userController');
+const { register, login, verifyUser, resendVerificationEmail, forgotPassword, resetPassword } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/register', register);
 
-// router.post('/login', login);
+router.post('/login', login);
 
 router.get('/user-verify/:token', verifyUser);
+
+router.post('/forgot-password', forgotPassword);
+
+router.post('/initiate/recover/:token', resetPassword);
 
 // router.post('/resend-verification/', resendVerificationEmail);
 
