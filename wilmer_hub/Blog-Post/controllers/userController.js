4d66993e57de4ca4fe19cbc30a44947c879f5c3f
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-26 14:27:55 +0100

    Verify added

--- wilmer_hub/Blog-Post/controllers/userController.js
+++ wilmer_hub/Blog-Post/controllers/userController.js
@@ -47,37 +47,114 @@ exports.register = async (req, res) => {
     }
 }
 
-exports.login = async (req, res) => {
-    // Extract the Email and Password from the request body
-    const { email, password } = req.body;
-    // First thing is to check if the user is registred in the database
-    const user = await userModel.findOne({email: email.toLowerCase()});
-    // Throw an Error is the User is not existing
-    if(user === null){
-        return res.status(404).json({
-            message: `User with Email: ${email} does not exist.`
-        })
-    };
-    // Conpare the password if it corresponds with the one saved in the database
-    const passwordCorrect = await bcrypt.compare(password, user.password);
-    // Throw an error if the password does not match
-    if (passwordCorrect === false){
-        return res.status(400).json({
-            message: 'Incorrect password entered'
-        })
-    };
-    const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
-    // const {password, ...userData} = user
+// Verify User
+exports.verifyUser = async (req, res) => {
+    try {
+        const { token } = req.params;
+        if (!token) {
+            return res.status(400).json({
+                message: 'Token not found'
+            })
+        };
 
-    // Send a success response
-    res.status(200).json({
-        message: 'Login successful',
-        data: {
-            fullName: user.fullName,
-            email: user.email,
-            id: user._id,
-            profilePic: user.profilePic,
-        },
-        token
-    })
+        jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+            if (err) {
+                if (err instanceof jwt.JsonWebTokenError) {
+                    const decodedToken = jwt.decode(token)
+                    const user = await userModel.findById(decodedToken.userId);
+                    if (user === null) {
+                        return res.status(404).json({
+                            message: 'User not found'
+                        })
+                    }
+
+                    if (user.isVerified === true) {
+                        return res.status(404).json({
+                            message: 'User has already been verified'
+                        })
+                    }
+
+                    const newToken = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '5mins' });
+                    const link = `${req.protocol}://${req.get('host')}/api/v1/user-verify/${newToken}`;
+                    // Dynamically get the User's firstname from the Fullname
+                    const firstName = user.fullName.split(' ')[0]
+
+                    // Configure the email sending details
+                    const mailDetails = {
+                        email: user.email,
+                        subject: 'Email Verification',
+                        html: signUpTemplate(link, firstName)
+                    }
+                    // Await nodemailer to send the user an email
+                    await sendEmail(mailDetails);
+
+                    res.status(200).json({
+                        message: 'Link expired, Check your email for the new verification link'
+                    });
+                }
+            } else {
+                console.log(payload)
+                const user = await userModel.findById(payload.userId);
+                if (user === null) {
+                    return res.status(404).json({
+                        message: 'User not found'
+                    })
+                }
+                if (user.isVerified === true) {
+                    return res.status(400).json({
+                        message: 'User has already been verified'
+                    })
+                }
+
+                user.isVerified = true;
+                await user.save();
+
+
+                res.status(200).json({
+                    message: 'User verified successfully'
+                })
+            }
+        })
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
 }
+
+// exports.login = async (req, res) => {
+//     // Extract the Email and Password from the request body
+//     const { email, password } = req.body;
+//     // First thing is to check if the user is registred in the database
+//     const user = await userModel.findOne({email: email.toLowerCase()});
+//     // Throw an Error is the User is not existing
+//     if(user === null){
+//         return res.status(404).json({
+//             message: `User with Email: ${email} does not exist.`
+//         })
+//     };
+//     // Conpare the password if it corresponds with the one saved in the database
+//     const passwordCorrect = await bcrypt.compare(password, user.password);
+//     // Throw an error if the password does not match
+//     if (passwordCorrect === false){
+//         return res.status(400).json({
+//             message: 'Incorrect password entered'
+//         })
+//     };
+//     const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+//     // const {password, ...userData} = user
+
+//     // Send a success response
+//     res.status(200).json({
+//         message: 'Login successful',
+//         data: {
+//             fullName: user.fullName,
+//             email: user.email,
+//             id: user._id,
+//             profilePic: user.profilePic,
+//         },
+//         token
+//     })
+// }
