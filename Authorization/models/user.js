66c8d29553824c203978a4de1a3928f2adb28c63
ahmeddavids <ahmeddavids6@gmail.com>
2025-02-27 17:24:56 +0100

    User model

---
+++ Authorization/models/user.js
@@ -0,0 +1,45 @@
+const mongoose  = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        require: true,
+        lowercase: true
+    },
+    username: {
+        type: String,
+        require: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female'],
+        require: true
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    scoreIds: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Scores"
+    }],
+    
+},{timestamps: true});
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
