cc87adabf022ccd1bd3ba51ad810695e9e68fe46
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-01 18:02:23 +0100

    Update

--- Authorization/middleware/authentication.js
+++ Authorization/middleware/authentication.js
@@ -5,12 +5,10 @@ exports.authenticate = async (req, res, next) => {
     try {
         // Get the Token from the authorization headers
         const auth = req.headers.authorization;
-        if (!auth) {return res.status(400).json({message: 'Token not found'})
-        }
+        if (!auth) {return res.status(400).json({message: 'Token not found'})}
         // Make sure the token is a valid JWT token
         const token = auth.split(' ')[1];
-        if (!token) {return res.status(400).json({message: 'Invalid Token'})
-        }
+        if (!token) {return res.status(400).json({message: 'Invalid Token'})}
         // Verify the token to be sure it's still valid
         const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
         // Check if the user is still existing
