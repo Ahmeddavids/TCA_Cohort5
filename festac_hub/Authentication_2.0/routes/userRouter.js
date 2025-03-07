d75dd105a2c701e650f3fc0593831ec2f671c929
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-07 23:39:22 +0100

    User router

--- festac_hub/Authentication_2.0/routes/userRouter.js
+++ festac_hub/Authentication_2.0/routes/userRouter.js
@@ -1,7 +1,18 @@
-const { register } = require('../controllers/userController');
+const { register, verifyUser, resendVerificationEmail, login, getAll, makeAdmin } = require('../controllers/userController');
+const { authenticate, superAdminAuth } = require('../middlewares/authentication');
 
 const router = require('express').Router();
 
 router.post('/register', register);
+
+router.get('/verify-user/:token', verifyUser);
+
+router.post('/login', login);
+
+router.post('/resend-verification', resendVerificationEmail);
+
+router.get('/users', authenticate, getAll);
+
+router.patch('/make-admin/:id', authenticate, superAdminAuth, makeAdmin);
 
 module.exports = router
