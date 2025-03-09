9983c6978c12072c22bb24d2965d8a9a50619d83
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-09 18:44:24 +0100

    Updated server

--- festac_hub/Authentication_2.0/server.js
+++ festac_hub/Authentication_2.0/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('./config/database');
 const userRouter = require('./routes/userRouter');
+const categoryRouter = require('./routes/categoryRouter');
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use('/api/v1', categoryRouter);
 app.use('/api/v1', userRouter);
 
 app.listen(PORT, ()=> {
