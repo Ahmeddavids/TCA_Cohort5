9a986c50b158be67151fa45ec4a115e125f6ec34
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-12 20:30:49 +0100

    Category updated

--- festac_hub/Authentication_2.0/routes/categoryRouter.js
+++ festac_hub/Authentication_2.0/routes/categoryRouter.js
@@ -1,7 +1,9 @@
-const { createCategory } = require('../controllers/categoryController');
+const { createCategory, getAll } = require('../controllers/categoryController');
 const { authenticate, adminAuth } = require('../middlewares/authentication');
 const router = require('express').Router();
 
 router.post('/category', authenticate, adminAuth, createCategory)
+
+router.get('/category', getAll)
 
 module.exports = router;
