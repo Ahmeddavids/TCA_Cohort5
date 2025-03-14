218fa33cb70a0e58dcbc15051466fc01a7f0f84d
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-14 16:18:12 +0100

    Transaction model

---
+++ Payment-Integration/models/transaction.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    reference: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Success', 'Failed'],
+        default: 'Pending'
+    },
+    paymentDate: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const transactionModel  = mongoose.model('Transactions', transactionSchema);
+
+module.exports = transactionModel;
