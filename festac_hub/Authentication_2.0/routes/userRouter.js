08183152438856e31984708b012218ae971ec26c
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-19 15:41:49 +0100

    Updated

--- festac_hub/Authentication_2.0/routes/userRouter.js
+++ festac_hub/Authentication_2.0/routes/userRouter.js
@@ -1,6 +1,8 @@
 const { register, verifyUser, resendVerificationEmail, login, getAll, makeAdmin } = require('../controllers/userController');
 const { authenticate, superAdminAuth } = require('../middlewares/authentication');
 const { registerValidation } = require('../middlewares/validator');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const router = require('express').Router();
 
@@ -15,5 +17,17 @@ router.post('/resend-verification', resendVerificationEmail);
 router.get('/users', authenticate, getAll);
 
 router.patch('/make-admin/:id', authenticate, superAdminAuth, makeAdmin);
+
+router.get('/google-authenticate', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/login', passport.authenticate('google'), async (req, res) => {
+    console.log('Req User: ', req.user)
+    const token = await jwt.sign({ userId: req.user._id, isVerified: req.user.isVerified }, process.env.JWT_SECRET, { expiresIn: '1day' });
+    res.status(200).json({
+        message: 'Google Auth Login Successful',
+        data: req.user,
+        token
+    });
+})
 
 module.exports = router
