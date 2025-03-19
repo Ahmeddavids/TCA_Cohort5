5e1a14bf3504c63dc60a2e1e64bb8296bb16d5c7
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-19 15:40:56 +0100

    Updated the the server with session and passport config

--- festac_hub/Authentication_2.0/server.js
+++ festac_hub/Authentication_2.0/server.js
@@ -4,13 +4,28 @@ const userRouter = require('./routes/userRouter');
 const categoryRouter = require('./routes/categoryRouter');
 const roomRouter = require('./routes/roomRouter');
 const PORT = process.env.PORT;
+const secret = process.env.EXPRESS_SESSION_SECRET;
+const session = require('express-session');
+const passport = require('passport');
+require('./middlewares/passport')
+
+
 const app = express();
 
 app.use(express.json());
+app.use(session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/api/v1', roomRouter);
 app.use('/api/v1', categoryRouter);
 app.use('/api/v1', userRouter);
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server is listening to PORT: ${PORT}`)
 })
