f844ddb1efd1a8876e8d260832d5c54dd6af83f4
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-27 14:23:52 +0100

    Updated server

--- wilmer_hub/Blog-Post/server.js
+++ wilmer_hub/Blog-Post/server.js
@@ -4,11 +4,15 @@ const PORT = process.env.PORT || 8080;
 const userRouter = require('./routes/userRouter');
 const postRouter = require('./routes/postRouter');
 const commentRouter = require('./routes/commentRouter');
+const replyRouter = require('./routes/replyRouter');
+const reactionRouter = require('./routes/reactionRouter');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api/v1',reactionRouter);
+app.use('/api/v1',replyRouter);
 app.use('/api/v1',commentRouter);
 app.use('/api/v1',postRouter);
 app.use('/api/v1',userRouter);
