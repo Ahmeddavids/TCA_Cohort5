55671b3911905bc51d5a9220d352a4a34f052e43
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-27 15:06:36 +0100

    Updated authentication

--- wilmer_hub/Blog-Post/middleware/authentication.js
+++ wilmer_hub/Blog-Post/middleware/authentication.js
@@ -1,25 +1,76 @@
-
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user');
 exports.authenticate = async (req, res, next) => {
     try {
-        const auth = req.headers.authorization;
-        if (!auth){
-            return res.status(400).json({
-                message: 'Authorization failed: No token passed'
-            })
-        }
-        const token = auth.split(' ')[1];
-        if (!token){
-            return res.status(403).json({
+        // Get the token from the request header
+        const auth = req.header('Authorization')?.split(' ')[1]
+        if (auth == undefined) {
+            return res.status(401).json({
                 message: 'Token not found'
             })
         }
+        // const token = auth.split(' ')[1];
 
-        
+        // if (token == undefined) {
+        //     return res.status(401).json({
+        //         message: 'Invalid Token'
+        //     })
+        // }
+        const decodedToken = await jwt.verify(auth, process.env.JWT_SECRET);
+        // Check for the user and throw error if not found
+        const user = await userModel.findById(decodedToken.userId);
+        if (user == null) {
+            return res.status(404).json({
+                message: 'Authentication Failed: User not found'
+            });
+        }
+
+        req.user = decodedToken;
+
+        next();
 
     } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({
+                message: 'Session timed-out, Please login to continue'
+            })
+        }
         console.log(error.message)
-      res.status(500).json({
-        message: 'Internal Server Error'
-      })  
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+exports.adminAuth = async (req, res, next) => {
+    try {
+        if(req.user.isAdmin == true){
+            next()
+        } else{
+            res.status(403).json({
+                message: "Unauthorized: Not an Admin"
+            })
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
+exports.superAdminAuth = async (req, res, next) => {
+    try {
+        if(req.user.isSuperAdmin == true){
+            next()
+        } else{
+            res.status(403).json({
+                message: "Unauthorized: You're not allowed to perform this action."
+            })
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
     }
 }
