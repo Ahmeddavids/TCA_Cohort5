871a79ac95f4aa52b891d5fac8f564185bf601c2
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-28 07:09:42 +0100

    Updated passport js

--- festac_hub/Authentication_2.0/middlewares/passport.js
+++ festac_hub/Authentication_2.0/middlewares/passport.js
@@ -5,7 +5,7 @@ const userModel = require('../models/user');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:1458/api/v1/auth/google/login"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
     async (accessToken, refreshToken, profile, cb) => {
         console.log("Profile: ", profile)
