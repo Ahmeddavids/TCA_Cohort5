6c5fe755536d363d17468546f3e2222f404db837
ahmeddavids <ahmeddavids6@gmail.com>
2025-04-01 13:36:35 +0100

    Updated

--- wilmer_hub/Intro_MongDB/config/database.js
+++ wilmer_hub/Intro_MongDB/config/database.js
@@ -1,7 +1,7 @@
 // Import Mongoose
 const mongoose = require('mongoose');
 // Connect to MongoDB URI
-mongoose.connect('mongodb+srv://ahmeddavids6:cohort5@cohort5.9vnvq.mongodb.net/first-mongoose-class')
+mongoose.connect('first-mongoose-class')
 .then(() => {
     console.log('Connection to database is successfull');
 })
